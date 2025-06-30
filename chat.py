a8c686b2e62e80c2b3382fdf33a45eb567e1beaf
Vincent Mugondora <166591818+VincentMugondora@users.noreply.github.com>
2025-06-30 12:24:57 +0000

    Refactor prompt generation for AI assistant and update model loading to use Orca Mini 3B

--- chat.py
+++ chat.py
@@ -1,35 +1,26 @@
 from typing import List
 from ctransformers import AutoModelForCausalLM
 
-# Load Llama2-7B-Chat model
 llm = AutoModelForCausalLM.from_pretrained(
-    "TheBloke/Llama-2-7B-Chat-GGUF",
-    model_file="llama-2-7b-chat.Q4_0.gguf"
+    "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
 )
 
-# Define prompt using Llama2 chat format
+
 def get_prompt(instruction: str, history: List[str] = None) -> str:
-    system = "You are an AI assistant that gives helpful answers. Answer with just the correct word, nothing more."
-    prompt = f"<s>[INST] <<SYS>>\n{system}\n<</SYS>>\n\n{instruction} [/INST]"
+    system = "You are an AI assistant that gives helpful answers. You answer the question in a short and concise way."
+    prompt = f"### System:\n{system}\n\n### User:\n"
+    if history is not None:
+        prompt += f"This is the conversation history: {''.join[history]}. Now answer the question.\n\n"
+    prompt += f"{instruction}\n\n### Response:\n"
     print(f"Prompt created: {prompt}")
     return prompt
 
-# Provide the question
-question = "The name of the capital of Zimbabwe?"
 
-# Generate prompt and pass it to the model
+history = []
+
+question = "Which city is the capital of India?"
+
 prompt = get_prompt(question)
 for word in llm(prompt, stream=True):
     print(word, end="", flush=True)
-
-print()
-
-# Provide the question
-question = "And which is of the United States?"
-
-# Generate prompt and pass it to the model
-prompt = get_prompt(question)
-for word in llm(prompt, stream=True):
-    print(word, end="", flush=True)
-
 print()
