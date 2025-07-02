dc3819a0f023134482c5be49075e11f59fd1da42
Vincent Mugondora <166591818+VincentMugondora@users.noreply.github.com>
2025-07-02 11:51:39 +0000

    Fix history check in prompt generation and update message handling in on_message

--- chat.py
+++ chat.py
@@ -5,7 +5,7 @@ from ctransformers import AutoModelForCausalLM
 def get_prompt(instruction: str, history: list[str] | None = None) -> str:
     system = "You are an AI assistant that gives helpful answers. You answer the question in a short and concise way."
     prompt = f"### System:\n{system}\n\n### User:\n"
-    if history is not None:
+    if len(history) > 0:
         prompt += f"This is the conversation history: {''.join(history)}. Now answer the question: "
     prompt += f"{instruction}\n\n### Response:\n"
     print(f"Prompt created: {prompt}")
@@ -14,17 +14,23 @@ def get_prompt(instruction: str, history: list[str] | None = None) -> str:
 
 @cl.on_message
 async def on_message(message: cl.Message):
+    message_history = cl.user_session.get("message_history", [])
+    message_history.append(message.content)
     msg = cl.Message(content="")
     await msg.send()
 
-    prompt = get_prompt(message.content)
+    prompt = get_prompt(message.content, message_history)
+    response = ""
     for word in llm(prompt, stream=True):
         await msg.stream_token(word)
+        response += word
     await msg.update()
+    message_history.append(response)
 
 
 @cl.on_chat_start
 async def on_chat_start():
+    cl.user_session.set("message_history", [])
     global llm
 
     llm = AutoModelForCausalLM.from_pretrained(
