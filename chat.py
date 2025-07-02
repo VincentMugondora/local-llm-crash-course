3c98fb54eaf5a1e67f5c66b577f253cb4bbf5652
Vincent Mugondora <166591818+VincentMugondora@users.noreply.github.com>
2025-07-02 11:23:12 +0000

    Rename app from "Chatbot" to "Hustlr" and update welcome message to address users as "human"

--- chat.py
+++ chat.py
@@ -1,17 +1,12 @@
 import chainlit as cl
-from typing import List
 from ctransformers import AutoModelForCausalLM
 
-llm = AutoModelForCausalLM.from_pretrained(
-    "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
-)
 
-
-def get_prompt(instruction: str, history: List[str] = None) -> str:
+def get_prompt(instruction: str, history: list[str] | None = None) -> str:
     system = "You are an AI assistant that gives helpful answers. You answer the question in a short and concise way."
     prompt = f"### System:\n{system}\n\n### User:\n"
     if history is not None:
-        prompt += f"This is the conversation history: {''.join(history)}. Now answer the question.\n\n"
+        prompt += f"This is the conversation history: {''.join(history)}. Now answer the question: "
     prompt += f"{instruction}\n\n### Response:\n"
     print(f"Prompt created: {prompt}")
     return prompt
@@ -19,25 +14,20 @@ def get_prompt(instruction: str, history: List[str] = None) -> str:
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    response = f"Hello, you just sent: {message.content}!"
-    await cl.Message(response).send()
+    msg = cl.Message(content="")
+    await msg.send()
 
-# history = []
-
-# question = "Which city is the capital of India?"
-
-# answer = ""
-# prompt = get_prompt(question)
-# for word in llm(prompt, stream=True):
-#     print(word, end="", flush=True)
-#     answer += word
-# print()
-
-# history.append(answer)
+    prompt = get_prompt(message.content)
+    for word in llm(prompt, stream=True):
+        await msg.stream_token(word)
+    await msg.update()
 
 
-# question = "And which is of Zimbabwe?"
+@cl.on_chat_start
+async def on_chat_start():
+    global llm
 
-# for word in llm(get_prompt(question, history)):
-#     print(word, end="", flush=True)
-# print()
+    llm = AutoModelForCausalLM.from_pretrained(
+        "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
+    )
+    await cl.Message("Model initialized. How can I help you?").send()
