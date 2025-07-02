a19a1615fecf009f3339a23765e30547d8370f25
Vincent Mugondora <166591818+VincentMugondora@users.noreply.github.com>
2025-07-02 13:11:42 +0000

    Refactor long_chain_demo.py to use PromptTemplate and LLMChain for improved prompt handling

--- long_chain_demo.py
+++ long_chain_demo.py
@@ -1,4 +1,6 @@
-from longchain_community.llms import CTransformers
+from langchain_community.llms import CTransformers
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 llm = CTransformers(
     model="zoltanctoth/orca_mini_3B-GGUF",
@@ -7,6 +9,10 @@ llm = CTransformers(
     max_new_tokens=20,
 )   
 
-prompt = f"### System:\nYou are an AI assistant that gives helpful answers. You answer the question in a short and concise way.\n\n### User:\n{instruction}\n\n### Response:\n"
+prompt_template = """### System:\nYou are an AI assistant that gives helpful answers. You answer the question in a short and concise way.
+\n\n### User:\n{instruction}\n\n### Response:\n"""
 
-print(llm.invoke("Which is the capital of France?"))
+prompt = PromptTemplate(template=prompt_template, input_variables=["instruction"])
+
+chain = LLMChain(llm = llm, prompt = prompt)
+print(chain.invoke({"instruction": "Which is the capital of France?"}))
