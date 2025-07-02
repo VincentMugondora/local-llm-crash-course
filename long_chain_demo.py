932dbce291c1d83474d63e51e89ce8c1aab865f3
Vincent Mugondora <166591818+VincentMugondora@users.noreply.github.com>
2025-07-02 12:50:02 +0000

    Add implementation of CTransformers in long_chain_demo.py

---
+++ long_chain_demo.py
@@ -0,0 +1,12 @@
+from longchain_community.llms import CTransformers
+
+llm = CTransformers(
+    model="zoltanctoth/orca_mini_3B-GGUF",
+    model_file="orca-mini-3b.q4_0.gguf",
+    model_type="llama2",
+    max_new_tokens=20,
+)   
+
+prompt = f"### System:\n{system}\n\n### User:\n{instruction}\n\n### Response:\n"
+
+print(llm.invoke("Which is the capital of France?"))
